0fc8e5024402cf5bdb5a692bae4af6740814eb70
Roshan <roshan@Geethas-MacBook-Pro.local>
2017-02-25 18:05:01 -0500

    Edges implemented.

---
+++ edge.java
@@ -0,0 +1,31 @@
+import Java.util.*;
+
+public class Edge {
+	private int vertex1;
+	private int vertex;
+	private float weight;
+
+	public Edge(int v1, int v2, float w) {
+		if (v2<v1) {
+			this.vertex1 = v2;
+			this.vertex2 = v1;
+		}
+		else {
+			this.vertex1 = v1;
+			this.vertex2 = v2;	
+		}
+		this.weight = w;
+	}
+
+	public int getv1() {
+		return vertex1;
+	}
+
+	public int getv2() {
+		return vertex2;
+	}
+
+	public int getweight() {
+		return weight;
+	}
+}
